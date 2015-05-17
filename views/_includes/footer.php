d7215695877896c72692a0070da42b6a4568c1bf
Henrique Lopes <henriqdp@gmail.com>
2015-05-17 04:08:21 -0300

    trying again

--- views/_includes/footer.php
+++
@@ -1,7 +0,0 @@
-		<div id="footer">
-			<p><strong>Prefeitura</strong> - Rua Tal, 385 - Algum Bairro, Cidade / UF - CEP</p>
-		</div>
-	</div>
-	<script src="views/_js/jquery.min.js"></script>
-</body>
-</html>
